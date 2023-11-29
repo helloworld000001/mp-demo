67ce2f902d18faebf5e02b5bf8c7799e7dcb2609
root <2032461441@qq.com>
2023-11-29 13:45:35 +0800

    feat:使用wrapper条件构造器

--- src/test/java/com/itheima/mp/mapper/UserMapperTest.java
+++ src/test/java/com/itheima/mp/mapper/UserMapperTest.java
@@ -1,10 +1,15 @@
 package com.itheima.mp.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.itheima.mp.domain.po.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.lang.reflect.Array;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
@@ -61,5 +66,57 @@ class UserMapperTest {
     @Test
     void testDeleteUser() {
         userMapper.deleteById(6L);
+    }
+
+    @Test
+    void testQueryWrapper(){
+        // 查询出名字带o，存款大于等于1000的人的id username info balance
+        QueryWrapper<User> wrapper = new QueryWrapper<User>()
+                .select("id", "username", "info", "balance")
+                .like("username", "o")
+                .ge("balance", 1000);
+
+        List<User> users = userMapper.selectList(wrapper);
+        System.out.println(users);
+    }
+
+    @Test
+    void testQueryLambdaWrapper(){
+        // 使用Lambda取代字符串硬编码：推荐使用
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<User>()
+                .select(User::getId, User::getUsername, User::getInfo, User::getBalance)
+                .like(User::getUsername, "o")
+                .ge(User::getBalance, 1000);
+
+        List<User> users = userMapper.selectList(wrapper);
+        System.out.println(users);
+    }
+
+    /**
+     * 使用QueryWrapper的update操作
+     */
+    @Test
+    void testUpdateByQueryWrapper(){
+        // 更新用户名为Jack的用户余额是2000
+        User user = new User();
+        user.setBalance(2000);
+
+        QueryWrapper<User> wrapper = new QueryWrapper<User>()
+                .eq("username", "jack");
+
+        // 这里传入的user是要更新的数据封装到一个对象中
+        userMapper.update(user, wrapper);
+    }
+
+    @Test
+    void testUpdateWrapper(){
+        // 更新id为1，2，4的用户余额，都扣200
+        List<Long> ids = Arrays.asList(1L, 2L, 4L);
+        UpdateWrapper<User> wrapper = new UpdateWrapper<User>()
+                .setSql("balance = balance - 200")
+                .in("id", ids);
+
+        // 要更新的内容在wrapper写，实体就设置为null
+        userMapper.update(null, wrapper);
     }
 }
