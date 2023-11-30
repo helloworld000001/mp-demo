3b643b83cddd5c213c8445913c9e7327813f0a8d
root <2032461441@qq.com>
2023-11-30 21:36:15 +0800

    feat:使用自动化生成代码Address

---
+++ src/main/java/com/itheima/mp/mapper/AddressMapper.java
@@ -0,0 +1,16 @@
+package com.itheima.mp.mapper;
+
+import com.itheima.mp.domain.po.Address;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author chen
+ * @since 2023-11-29
+ */
+public interface AddressMapper extends BaseMapper<Address> {
+
+}
